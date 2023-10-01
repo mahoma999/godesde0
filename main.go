9f0850244718a52bd1b9f32be9494485ebb280d4
Oscar Reyes Avila <oscar_militar@hotmail.com>
2023-10-01 15:23:20 -0600

    Agregamos archivos

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+import(
+  "fmt"
+  "github.com/mahoma999/godesde0/variables"
+  
+)
+func main(){
+  fmt.Printf("Hola desde arch\n")
+  variables.VariablesEnteros()
+}
---
+++ variables/enteros.go
@@ -0,0 +1,11 @@
+package variables
+import "fmt"
+func VariablesEnteros(){
+  var numero int
+  varde32:=int32(9)
+  varde64:=int64(9)
+  fmt.Println("numero: ",numero)
+  fmt.Println("varde32: ",varde32)
+  fmt.Println("varde64: ",varde64)
+  
+}
